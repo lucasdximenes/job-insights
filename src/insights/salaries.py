d38b82d5513e48d6fef0fa87532a6607fcf01481
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-28 15:07:34 -0300

    feat: :rocket: implement `get_min_salary` function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -43,7 +43,15 @@ def get_min_salary(path: str) -> int:
     int
         The minimum salary paid out of all job opportunities
     """
-    raise NotImplementedError
+    job_list = read(path)
+    min_salary = min(
+        [
+            int(job["min_salary"])
+            for job in job_list
+            if job["min_salary"].isdigit()
+        ]
+    )
+    return min_salary
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
