39dd455c3066253758bd8f7d1f522242701ad260
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-28 15:11:22 -0300

    feat: :rocket: implement `filter_by_job_type` function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -60,4 +60,5 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    jobs_by_type = [job for job in jobs if job["job_type"] == job_type]
+    return jobs_by_type
