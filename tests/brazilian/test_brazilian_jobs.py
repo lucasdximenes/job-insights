ef33a219935efad3cd18587e01e73efaff62ae59
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-29 11:09:02 -0300

    test: :white_check_mark: implement `read_brazilian_file` test

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,46 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from unittest.mock import patch
+
+'''
+def read_brazilian_file(path: str) -> List[Dict]:
+    """Reads a portuguese file from a given path and returns its contents
+
+    Parameters
+    ----------
+    path : str
+        Full path to file
+
+    Returns
+    -------
+    list
+        List of rows as dicts
+    """
+    dict_jobs = jobs.read(path)
+    for job in dict_jobs:
+        job["title"] = job.pop("titulo")
+        job["salary"] = job.pop("salario")
+        job["type"] = job.pop("tipo")
+
+    return dict_jobs
+'''
+
+
+mock_jobs = [
+    {
+        "titulo": "titulo",
+        "salario": "salario",
+        "tipo": "tipo",
+    }
+]
 
 
 def test_brazilian_jobs():
-    pass
+    with patch("src.insights.jobs.read", return_value=mock_jobs) as mock_read:
+        assert read_brazilian_file("path") == [
+            {
+                "title": "titulo",
+                "salary": "salario",
+                "type": "tipo",
+            }
+        ]
+        mock_read.assert_called_once_with("path")
