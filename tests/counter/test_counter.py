51806f5362eb8439daebbd9faf50619e2ab46a76
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-29 10:54:16 -0300

    test: :white_check_mark: implement `counter_occurences` test

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+
+"""
+def count_ocurrences(path: str, word: str) -> int:
+    file = open(path, "r")
+    read_data = file.read()
+    word_count = read_data.lower().count(word.lower())
+    return word_count
+"""
 
 
 def test_counter():
-    pass
+    with patch(
+        "builtins.open", mock_open(read_data="Hello World!")
+    ) as mock_file:
+        assert count_ocurrences("path", "hello") == 1
+        mock_file.assert_called_once_with("path", "r")
